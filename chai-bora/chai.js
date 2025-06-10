516231253a8a5b8091c8dd42ff83073cfee1be1e
eberriphath <riphatheber@gmail.com>
2025-06-10 11:52:17 +0300

    chai bora javascript and html

---
+++ chai-bora/chai.js
@@ -0,0 +1,20 @@
+//function for chai ingredients
+
+function calculateChaiIngredients(numberOfCups){
+    const teaLeaves = numberOfCups
+    const water = numberOfCups * 200
+    const sugar = numberOfCups * 2
+    const milk = numberOfCups * 50
+//Amount of ingredients should change with the number of cups prompted.
+
+    //output for the ingredients
+    console.log(`To make ${numberOfCups} cups of kenyan chai,you will need:`)
+    console.log(`water: ${water} ml,`)
+    console.log(`milk: ${milk} ml,`)
+    console.log(`Tea leaves (MAJANI) ${teaLeaves} tablespoons`)
+    console.log(`Sugar (SUKARI): ${sugar} teaspoons.`)
+    console.log("BARAKA TELE ENJOY!!!")
+}
+
+const userInput = prompt("Karibu! How many cups of Chai Bora would you like to make?");
+calculateChaiIngredients(Number(userInput));
